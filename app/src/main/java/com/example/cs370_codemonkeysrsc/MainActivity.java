3512139305c31c4d4103e08bd857baaa20816c8e
leto42 <seaug3@yahoo.com>
2019-11-01 16:34:51 -0700

    Like and Favorite Page Layout

--- app/src/main/java/com/example/cs370_codemonkeysrsc/MainActivity.java
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.cs370_codemonkeysrsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LikeFavoritePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
