b09c90c745788391789f4375dacde0f8598400d3
leto42 <seaug3@yahoo.com>
2019-11-01 16:17:59 -0700

    Youtube Page Layout

---
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/YoutubePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs370_codemonkeysrsc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YoutubePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_page);
+    }
+}
