f2a9840d6a51553d8122eb0a6cd6c4ddd653e4e1
student <student@ssu-cs-dar28-167-218.sonoma.edu>
2019-11-11 18:08:39 -0800

    Added Home button to YoutubePage and added functionality to New, Home buttons on YoutubePageActivity

--- app/src/main/java/com/example/cs370_codemonkeysrsc/YoutubePageActivity.java
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/YoutubePageActivity.java
@@ -2,13 +2,36 @@ package com.example.cs370_codemonkeysrsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class YoutubePageActivity extends AppCompatActivity {
+    private Button new_button; // designed to return to input page *
+    private Button home_button; // designed to return to main page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_page);
+
+        home_button = findViewById(R.id.home_button);
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(YoutubePageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        new_button = findViewById(R.id.new_button);
+        new_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(YoutubePageActivity.this, GenrePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
