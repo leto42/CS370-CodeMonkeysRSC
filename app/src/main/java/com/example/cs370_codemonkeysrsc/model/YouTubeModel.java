6602f6f6ca3ae9af0610df5aa182b862566963cd
leto42 <seaug3@yahoo.com>
2019-11-22 18:40:48 -0800

    added youtube model and parser

---
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/model/YouTubeModel.java
@@ -0,0 +1,45 @@
+package com.example.cs370_codemonkeysrsc.model;
+
+public class YouTubeModel {
+
+    public String getVideoID() {
+        return videoID;
+    }
+
+    public void setVideoID(String videoID) {
+        this.videoID = videoID;
+    }
+
+    private String videoID;
+}
+
+// sample of partial JSON object
+
+/*
+"items" : {
+  {
+  "kind": "youtube#searchResult",
+  "etag": etag,
+  "id": {
+    "kind": string,
+    "videoId": string,
+    "channelId": string,
+    "playlistId": string
+  },
+  "snippet": {
+    "publishedAt": datetime,
+    "channelId": string,
+    "title": string,
+    "description": string,
+    "thumbnails": {
+      (key): {
+        "url": string,
+        "width": unsigned integer,
+        "height": unsigned integer
+      }
+    },
+    "channelTitle": string,
+    "liveBroadcastContent": string
+  }
+}
+ */
---
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/utility/YouTubeParser.java
@@ -0,0 +1,52 @@
+package com.example.cs370_codemonkeysrsc.utility;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.cs370_codemonkeysrsc.model.YouTubeModel;
+
+public class YouTubeParser {
+    // TAG is used in logging, to help trace where output comes from
+    private static final String TAG = "YouTubeParser";
+
+    /**
+     * Returns a list of recipes that match a search term
+     * @param json String json representing the server's response
+     * @return list of recipes (as YouTubeModels)
+     */
+    public static List<YouTubeModel> getMatches(String json) {
+
+        List<YouTubeModel> modelList = new ArrayList<>();
+
+        try {
+            // a single JSONObject representing the whole response
+            JSONObject response = new JSONObject(json);
+            JSONArray matchArray = new JSONArray();
+
+            matchArray = response.getJSONArray("items");
+            // Only need first object.
+            JSONObject video = matchArray.getJSONObject(0);
+
+            // get into nested JSON list
+            JSONObject ID_list = video.getJSONObject("id");
+
+            // get the video id needed to play the video.
+            String VideoID = ID_list.getString("videoId");
+
+            YouTubeModel youtubeModel = new YouTubeModel();
+            youtubeModel.setVideoID(VideoID);
+            modelList.add(youtubeModel);
+
+        } catch (JSONException e) {
+            Log.e(TAG, "getMatches: error parsing JSON", e);
+        }
+
+        return modelList;
+    }
+}
