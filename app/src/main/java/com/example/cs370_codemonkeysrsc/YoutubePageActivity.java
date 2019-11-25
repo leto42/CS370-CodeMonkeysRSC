b321fc8a66fc66a55b016bf5012f5c621dcb77db
student <student@ssu-cs-dar28-167-220.sonoma.edu>
2019-11-24 23:57:42 -0800

    Added Deezer API functionality (in dev)

--- app/src/main/java/com/example/cs370_codemonkeysrsc/YoutubePageActivity.java
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/YoutubePageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,6 +14,11 @@ public class YoutubePageActivity extends AppCompatActivity {
     private Button new_button; // designed to return to input page *
     private Button home_button; // designed to return to main page
     private Button like_button; // designed to save song to favorites
+    private String returnedSongTitle;
+    private int GenreID; //ORIGINAL
+    //static int GenreID;
+
+    public int getGenreID() {return GenreID;} // not currently working
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,36 @@ public class YoutubePageActivity extends AppCompatActivity {
         home_button = findViewById(R.id.home_button);
         new_button = findViewById(R.id.new_button);
         like_button = findViewById(R.id.like_button);
-        String GenreID = getIntent().getStringExtra("GENRE_ID");
+
+        // String GenreID = getIntent().getStringExtra("GENRE_ID"); ORIGINAL
+        GenreID = getIntent().getIntExtra("GENRE_ID", 0);
+        Log.d("YoutubePageActivity","receivedGenreID: " + GenreID); // checking that GenreID successfully received. (yes)
+
+        Deezer deezerSearch = new Deezer();
+        // *** need to get GenreID to Deezer class ***
+
+        /*
+        Intent intent = new Intent(YoutubePageActivity.this, Deezer.class);
+        intent.putExtra("GENRE_ID", GenreID);
+        startActivity(intent);
+        */
+
+        deezerSearch.setListener(new Deezer.DeezerListener() {
+            @Override
+            public void onDeezerCallback(String songTitle) {
+                returnedSongTitle = songTitle;
+                Log.d("YoutubePageActivity", "returnedSongTitle: " + returnedSongTitle);
+            }
+        });
+        Log.d("YoutubePageActivity", "returnedSongTitle: " + returnedSongTitle);
+        // Temporary display message to see the genre id being selected.
+        Context context = getApplicationContext();
+        CharSequence text = returnedSongTitle;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
 
         home_button.setOnClickListener(new View.OnClickListener() {
             @Override
