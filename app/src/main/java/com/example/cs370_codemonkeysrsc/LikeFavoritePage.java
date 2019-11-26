1cdd76188986a8f904181c489d019e3c4740eab9
skysurveil <46916978+skysurveil@users.noreply.github.com>
2019-11-25 21:38:41 -0800

    Revert "Design Adjustments"

---
+++ app/src/main/java/com/example/cs370_codemonkeysrsc/LikeFavoritePage.java
@@ -0,0 +1,14 @@
+package com.example.cs370_codemonkeysrsc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LikeFavoritePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like_favorite_page);
+    }
+}
